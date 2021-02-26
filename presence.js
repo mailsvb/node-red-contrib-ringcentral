ccff59d99a35333f1f8b240b35d8d64012e4aa8d
Sven Beisiegel <sven.beisiegel@atos.net>
2021-02-26 08:18:54 +0100

    add presence node and release version 1.3.0

---
+++ presence.js
@@ -0,0 +1,59 @@
+module.exports = function(RED) {
+    function GetPresence(n) {
+        RED.nodes.createNode(this, n);
+        const node = this;
+        node.creds = n.rccreds;
+        node.credsNode = RED.nodes.getNode(node.creds);
+        node.source = n.source || 'user';
+
+        node.on('input', function(msg, send, done) {
+            let status = {};
+            const getPresenceState = () => {
+                node.status({fill:"green",shape:"ring",text:"sending"});
+
+                let url = '/restapi/v1.0/account/~';
+                if (node.source === 'user') {
+                    url += '/extension/';
+                    if (msg.payload && msg.payload.userId && typeof msg.payload.userId === 'string') {
+                        url += msg.payload.userId;
+                    } else {
+                        url += '~';
+                    }
+                }
+                url += '/presence?detailedTelephonyState=true&sipData=true';
+                node.log(`path[${url}]`);
+                let error = false;
+                node.credsNode.get(url, (data) => {
+                    node.status({fill:"green",shape:"dot",text:"sent"});
+                    setTimeout(() => {
+                        node.status({});
+                    }, 2500);
+                    if (data) {
+                        msg.presence = data && data.records || data;
+                        send(msg);
+                    }
+                });
+            };
+
+            if( !node.credsNode.platformReady ) {
+                node.credsNode.addEventListener('rc-ready', function(evt) {
+                    getPresenceState();
+                })
+            }
+            else {
+                getPresenceState();
+            }
+            
+        });
+
+        node.on('close', function(removed, done) {
+            if (removed) {
+                // This node has been deleted
+            } else {
+                // This node is being restarted
+            }
+            done();        
+        });
+    }
+    RED.nodes.registerType("presence", GetPresence);
+}
